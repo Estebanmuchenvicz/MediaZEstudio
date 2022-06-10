58bddde8227bbe7892aaada3a012ea967c29c1dd
Estebanmuchenvicz <esteban.muchenvicz@gmail.com>
2022-06-10 03:03:02 -0300

    actualizacion

--- script/index.js
+++ script/index.js
@@ -1,6 +1,16 @@
-const menuToggle = document.querySelector (".menu-toggle")
-const menuPrincipal = document.querySelector (".menu-principal")
+const menuToggle = document.querySelector (".menu-toggle");
+const menuPrincipal = document.querySelector (".menu-principal");
 
 menuToggle.addEventListener("click", () =>{
     menuPrincipal.classList.toggle("nav-menu_visible");
 });
+
+/*window.onscroll = function(){
+    scroll = document.documentElement.scrollTop;
+
+    hedersuperior = document.getElementsByClassName(".heder__superior");
+
+    if(scroll > 3){
+        hedersuperior.classList.add(".header_mod")
+    }
+}*/
